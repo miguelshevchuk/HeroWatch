0be9a89e3a2e0001ab08aa040539cb7dc9778534
Migue <shevchuk.miguel@gmail.com>
2025-11-23 18:11:12 -0300

    Commit inicial del gateway

---
+++ gateway-gha/src/main/java/ar/com/gha/gatewaygha/GatewayGhaApplication.java
@@ -0,0 +1,13 @@
+package ar.com.gha.gatewaygha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayGhaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayGhaApplication.class, args);
+    }
+
+}
---
+++ gateway-gha/src/test/java/ar/com/gha/gatewaygha/GatewayGhaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.gha.gatewaygha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayGhaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
