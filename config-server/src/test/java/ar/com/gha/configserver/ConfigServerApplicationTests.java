da424f512212ce911ec34023afb2440abd2dda7b
Migue <shevchuk.miguel@gmail.com>
2025-11-23 18:30:48 -0300

    config-server

---
+++ config-server/src/test/java/ar/com/gha/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.gha.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
